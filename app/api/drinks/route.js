9a7b390747bee7216786180f4f8c393f0ae6c029
Mark Corbin <corb0140@algonquinlive.com>
2024-03-31 19:55:11 -0400

    test

--- app/api/drinks/route.js
+++
@@ -1,8 +0,0 @@
-export function GET(_req, res) {
-  res
-    .status(200)
-    .json(
-      { id: 160890, name: "Mark", message: "GET request successful" },
-      { Headers: { "content-type": "application/json" } }
-    );
-}

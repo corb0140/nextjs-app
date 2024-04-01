84e828e155dea2d60b0222abca5d85f8795c6065
Mark Corbin <corb0140@algonquinlive.com>
2024-03-31 21:47:36 -0400

    css update

--- app/components/Masthead/MastHead.js
+++ app/components/Masthead/MastHead.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const MastHead = () => {
   return (
-    <header className="container max-w-full flex justify-center align-middle bg-purple-950 py-4 text-2xl">
+    <header className="container max-w-full flex justify-center align-middle bg-purple-950 p-4 text-xl text-center md:text-2xl">
       <Link href="/">
         <h1>The Perfect Food & Movie For Today</h1>
       </Link>

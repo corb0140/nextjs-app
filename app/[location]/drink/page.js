3e4840deca5dec39443c93faec3eb3de3aa1f9f8
Mark Corbin <corb0140@algonquinlive.com>
2024-04-02 15:36:08 -0400

    update drink styling and functionality

--- app/[location]/drink/page.js
+++ app/[location]/drink/page.js
@@ -6,13 +6,18 @@ import DrinkCard from "../../components/DrinkCard/DrinkCard";
 import { useSearchParams, useRouter } from "next/navigation";
 
 const page = (props) => {
-  let location = decodeURIComponent(props.params.location);
-  const [drink, setDrink] = useState({});
   const router = useRouter();
+  let location = decodeURIComponent(props.params.location);
+
+  const [drink, setDrink] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
   const searchParams = useSearchParams();
   const query = searchParams.get("main");
 
   useEffect(() => {
+    setIsLoading(true);
+
     fetch(`http://localhost:3000/api/drinks?${query}`)
       .then((response) => {
         if (!response.ok) {
@@ -28,30 +33,39 @@ const page = (props) => {
           fetch(`https://www.thecocktaildb.com/api/json/v1/1/random.php`)
             .then((response) => response.json())
             .then((data) => {
-              setDrink({
-                title: data.drinks[0].strDrink,
-                id: data.drinks[0].idDrink,
-                src: data.drinks[0].strDrinkThumb,
-              });
+              setDrink(
+                data.drinks
+                //   {
+                //   title: data.drinks[0].strDrink,
+                //   id: data.drinks[0].idDrink,
+                //   src: data.drinks[0].strDrinkThumb,
+                // }
+              );
             });
         } else {
-          setDrink({
-            title: data.drinks[0].strDrink,
-            id: data.drinks[0].idDrink,
-          });
+          setDrink(
+            data.drinks
+            //   {
+            //   title: data.drinks[0].strDrink,
+            //   id: data.drinks[0].idDrink,
+            // }
+          );
         }
       })
       .catch((error) => {
         console.error(error);
+      })
+      .finally(() => {
+        setIsLoading(false);
       });
   }, [query]);
 
   return (
-    <div className="container max-w-full p-10 flex flex-col justify-center items-center">
+    <div className="container max-w-full p-10 flex flex-col justify-center">
       <h2 className="pb-3">
         Ingredients for <span className="text-teal-300">{query}</span>
       </h2>
-      <DrinkCard drink={drink} location={location} />
+      <DrinkCard drinks={drink} location={location} isLoading={isLoading} />
     </div>
   );
 };
--- app/components/DrinkCard/DrinkCard.js
+++ app/components/DrinkCard/DrinkCard.js
@@ -4,9 +4,13 @@ import classes from "./DrinkCard.module.css";
 
 import { useRouter } from "next/navigation";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
-const DrinkCard = ({ drink, location }) => {
+import Loader from "../Loader/Loader";
+
+const DrinkCard = ({ drinks, location, isLoading }) => {
   const router = useRouter();
+  const [loading, setLoading] = useState(true);
 
   const back = () => {
     router.back();
@@ -16,30 +20,51 @@ const DrinkCard = ({ drink, location }) => {
     router.push("/");
   };
 
+  useEffect(() => {
+    if (drinks.length > 0) {
+      setLoading(isLoading);
+    }
+  }, [drinks]);
+
   return (
-    <div className={classes.card}>
-      <h1 className={classes.heading}>{drink.title}</h1>
-
-      <img className={classes.image} src={drink.src} alt={drink.title} />
-
-      <Link
-        className={classes.ingredientsLink}
-        href={`https://www.thecocktaildb.com/drink/${drink.id}`}
-        target="_blank"
-      >
-        Click to see ingredients
-      </Link>
-
+    <>
       <div className={classes.buttonContainer}>
-        <button className={classes.button} onClick={back}>
+        <button className={classes.button} onClick={(ev) => back(ev)}>
           Back to {location}
         </button>
 
-        <button className={classes.button} onClick={home}>
+        <button className={classes.button} onClick={(ev) => home(ev)}>
           New Location
         </button>
       </div>
-    </div>
+
+      {loading && <Loader />}
+
+      <ul className={classes.cardList}>
+        {drinks.length > 0 &&
+          drinks.map((drink) => {
+            return (
+              <div className={classes.card} key={drink.idDrink}>
+                <h1 className={classes.heading}>{drink.strDrink}</h1>
+
+                <img
+                  className={classes.image}
+                  src={drink.strDrinkThumb}
+                  alt={drink.strDrink}
+                />
+
+                <Link
+                  className={classes.ingredientsLink}
+                  href={`https://www.thecocktaildb.com/drink/${drink.idDrink}`}
+                  target="_blank"
+                >
+                  Click to see ingredients
+                </Link>
+              </div>
+            );
+          })}
+      </ul>
+    </>
   );
 };
 

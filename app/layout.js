cb6ebd61d8a1d3c0f3e7902ffa697e5f2d6a60eb
Mark Corbin <corb0140@algonquinlive.com>
2024-04-02 00:15:11 -0400

    update tailwind on body

--- app/layout.js
+++ app/layout.js
@@ -4,9 +4,9 @@ import MastHead from "./components/Masthead/MastHead";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="font-Montserrat">
+      <body className="font-Montserrat h-screen">
         <MastHead />
-        <main className="">{children}</main>
+        <main>{children}</main>
       </body>
     </html>
   );

e60fcf3a45e7a06ad1c211f0bd8f7a3abae87dbf
Mark Corbin <corb0140@algonquinlive.com>
2024-04-02 23:42:12 -0400

    testing

--- app/api/weather/route.js
+++ app/api/weather/route.js
@@ -4,34 +4,40 @@ export async function GET(request) {
   const locale = query.get("locale");
   const apiKey = process.env.WEATHER_API_KEY;
 
-  try {
-    const res = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${locale}&appid=${apiKey}&units=metric`,
-      {
-        method: "GET",
-        headers: {
-          accept: "application/json",
-        },
-      }
-    );
-
-    if (!res.ok) {
-      throw new Error("Failed to fetch data from the API");
+  const res = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${locale}&appid=${apiKey}&units=metric`,
+    {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+      },
     }
+  );
 
-    const data = await res.json();
-
-    return new Response(JSON.stringify(data), {
-      // headers: {
-      //   "content-type": "application/json",
-      //   "access-control-allow-methods": "GET,HEAD",
-      //   "access-control-allow-origin": "*",
-      // },
-      status: 200,
-    });
-  } catch (error) {
-    return new Response(error, {
-      status: 500,
-    });
+  if (!res.ok) {
+    throw new Error("Failed to fetch data from the API");
   }
+
+  const data = await res.json();
+
+  return new Response(JSON.stringify(data), {
+    headers: {
+      "content-type": "application/json",
+      "access-control-allow-methods": "GET,HEAD",
+      "access-control-allow-origin": "*",
+    },
+    status: 200,
+  });
+}
+
+export async function HEAD(request) {
+  return new Response(null, {
+    headers: {
+      "content-type": "application/json",
+      "content-length": 868,
+      "access-control-allow-methods": "GET,HEAD",
+      "access-control-allow-origin": "*",
+    },
+    status: 200,
+  });
 }
